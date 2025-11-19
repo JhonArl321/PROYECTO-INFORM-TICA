97b4f4e65013796ef38afbe12fd64cf4e81ad6e5
Juan <naujralios01@gmail.com>
2025-11-19 10:51:54 -0600

    corregir rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import AyudasPage from "./pages/AyudasPage";
