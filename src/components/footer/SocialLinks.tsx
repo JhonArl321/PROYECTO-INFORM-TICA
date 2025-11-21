f590cd559db76ff837588e5faeace2756e6222b9
Juan <naujralios01@gmail.com>
2025-11-20 18:46:43 -0600

    corregir iconos a redes sociles...agreagar pagina de la uvg altoplano

--- src/components/footer/SocialLinks.tsx
+++ src/components/footer/SocialLinks.tsx
@@ -1,12 +1,12 @@
-import { Facebook } from 'lucide-react';
+import { } from 'lucide-react';
 
 // muestra íconos de redes sociales con enlaces externos
 const SocialLinks: React.FC = () => {
   return (
     <div>
-      <h3 className="font-bold mb-4">Redes Sociales</h3>
+      <h3 className="font-bold mb-4">Pagina Oficial</h3>
       <div className="flex space-x-4">
-        <a href="https://altiplano.uvg.edu.gt/" target="_blank"><Facebook size={24} /></a>
+        <a href="https://altiplano.uvg.edu.gt/" target="_blank">visitar pagina</a>
     
       </div>
     </div>
